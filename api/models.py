34c9fbb5bb658c72d80ef5a8a2f8b658638e1ff6
Rodolfo Barbosa <rodolfo.barbosa@arizona.global>
2018-06-07 18:46:16 -0300

    Feito modelo component mais data inicial, e migrato para pagina de adm

--- api/models.py
+++ api/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Post(models.Model):
     component_name = models.CharField(max_length=200)
     inicio_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return self.inicio_date >= timezone.now() - datetime.timedelta(days=1)
