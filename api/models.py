824dca4576a235d80bc2dada2769433ed196146a
Rodolfo Barbosa <rodolfo.barbosa@arizona.global>
2018-06-08 09:38:40 -0300

    Corrido probelma com campo data nulo

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from tastypie.utils.timezone import now
 
 
 class Post(models.Model):
     component_name = models.CharField(max_length=200)
-    inicio_date = models.DateTimeField('date published')
+    inicio_date = models.DateTimeField(default=now)
     def was_published_recently(self):
         return self.inicio_date >= timezone.now() - datetime.timedelta(days=1)
