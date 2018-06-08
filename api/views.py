05b2587b7862e1aa3a959d4f8ad76391e015351d
Rodolfo Barbosa <rodolfo.barbosa@arizona.global>
2018-06-08 09:35:17 -0300

    Corrigido problema de autorizacao no POST

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from api.models import Post
 from tastypie.resources import ModelResource
+from tastypie.authorization import Authorization
 
 class PostResource(ModelResource):
     class Meta:
         resource_name = 'post'
         queryset = Post.objects.all()
+        authorization = Authorization()
